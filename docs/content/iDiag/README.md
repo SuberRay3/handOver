5e2e4add649be893f8ef2facc1bb3c1b3de8f83c
ray <ljl519525771@qq.com>
2019-04-19 16:16:39 +0800

    build: build version

--- docs/content/iDiag/README.md
+++
@@ -1,3 +0,0 @@
-# 智能诊断 ![](https://img.shields.io/badge/%E7%8A%B6%E6%80%81-%E5%B7%B2%E5%BA%9F%E5%BC%83-red.svg)
-
-已无法使用 -_- ！
--- publish.sh
+++
@@ -1,5 +0,0 @@
-rm -rf ./docs/.vuepress/dist
-npm run build;
-git add ./docs/.vuepress/dist --force;
-git commit -m "build: build version";
-git subtree push --prefix docs/.vuepress/dist origin gh-pages;
